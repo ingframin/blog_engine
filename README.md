3f2c0ed335c8e8cc57830c2830edc7daced49af0
Franco Minucci <franco.minucci@esat.kuleuven.be>
2019-02-05 09:35:43 +0100

    Added license and Readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# blog_engine
-A small blog engine written in python
+# Blog Engine
+A small blog engine written in python 3. It is based on CherryPy and SQLite
+(The db engine can be easily changed). 
+
+First run manage_db.py to generate the database.
+In the setup method you can put your username and password and email. These credentials are used by the admin interface to add posts. 
+This is a temporary solution, until I find time to write a proper db module.
+
+I put my logos here but you can change it, feel free also to modify the HTML templates.
+
+MY LOGOS ARE NOT FOR REUSE IN YOUR WEBSITE.
+
+All the code is redistributed as GPLv3.
--- manage_db.py
+++ manage_db.py
@@ -1,3 +1,20 @@
+'''
+Blog Engine
+Copyright (C) 2019  Franco Minucci
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 import sqlite3
 
 DB_STRING = './database/blog.db'
@@ -9,7 +26,7 @@ def setup():
         for stmt in ct:
             conn.execute(stmt)
     
-    conn.execute("INSERT INTO admins VALUES ('framin', 'Hx125vwZQS§','ir.fminucci@gmail.com')")
+    conn.execute("INSERT INTO admins VALUES ('username', 'password','email@whatever.xxx')")
     conn.commit()
     conn.close()
 
@@ -41,9 +58,9 @@ def query_by_year(table,year):
 
 if __name__=='__main__':
 
-    #setup()
+    setup()
     # p0 = '0,"My first post","This is the content of my first post","30-10-2018"'
     # p1 = '1,"My second post","This is the content of my second post","30-10-2018"'
     # insert('blog_post',[p0,p1])
-    print(query('admins'))
+    #print(query('admins'))
     # print(query('blog_post'))
